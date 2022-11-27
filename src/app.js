b3063ecd3e1a6570522d1e98f2dc6132989f18c7
Peter Milo <68414543+PeterMilo@users.noreply.github.com>
2022-11-27 09:21:42 +0100

    Added a mutation to form

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-import { DataStore } from '@aws-amplify/datastore';
-import { Customers } from './models';
-
-import { Amplify, API, graphqlOperation } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+// import { DataStore } from '@aws-amplify/datastore';
+// import { Customers } from './models';
 
 /*
 await DataStore.save(
@@ -14,10 +10,37 @@ await DataStore.save(
 );
 */
 
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { createCustomers } from './graphql/mutations';
+Amplify.configure(awsconfig);
+
 const createNewCustomer = async e => {
   e.preventDefault();
 
   const customer = {
-    name: document.getElementById('name').ariaValueMax,
+    name: document.getElementById('name').value,
   };
+
+  try {
+    // Make the API request: provide the createGif operation, provide the user's gif data
+    const newCustomer = await API.graphql(
+      graphqlOperation(createCustomers, { input: name })
+    );
+    // Print the data to the console once it comes back
+    console.log(newCustomer);
+    // Reset the form (make the fields blank again)
+    document.getElementById('create-form').reset();
+  } catch (err) {
+    // If the request fails, print the error message to the console
+    console.error(err);
+  }
 };
+
+// run our createNewGif function when the form is submitted
+document
+  .getElementById('create-form')
+  .addEventListener('submit', createNewCustomer);
+
+// Follow instructions for https://docs.amplify.aws/lib/graphqlapi/getting-started/q/platform/js/#create-the-graphql-api
+// Create etc in app.js
